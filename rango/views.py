807065194d159d79fec79bf0db897fc018629647
“Yigit <2795191s@student.gla.ac.uk>
2023-08-11 21:36:32 +0300

    Chapter 8 & fixes

--- rango/views.py
+++ rango/views.py
@@ -1,37 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category, Page
-from rango.forms import CategoryForm, PageForm
+from rango.models import Category
+from rango.models import Page
+from rango.forms import CategoryForm
 from django.shortcuts import redirect
 from django.urls import reverse
+from rango.forms import PageForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    context_dict['extra'] = 'From the model solution on GitHub'
 
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse('Rango says here is the about page.')
+    # Spoiler: you don't need to pass a context dictionary here.
+    return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
     context_dict = {}
 
     try:
         category = Category.objects.get(slug=category_name_slug)
-
-        # The filter() will return a list of page objects or an empty list.
         pages = Page.objects.filter(category=category)
 
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
-        context_dict['category'] = None
         context_dict['pages'] = None
-
+        context_dict['category'] = None
+    
     return render(request, 'rango/category.html', context=context_dict)
 
 def add_category(request):
@@ -41,25 +45,23 @@ def add_category(request):
         form = CategoryForm(request.POST)
 
         if form.is_valid():
-            # Save the new category to the database
             form.save(commit=True)
-            # Now that the category is saved, we could confirm this.
-            # For now, just redirect the user back to the index view.
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
-        
+    
     return render(request, 'rango/add_category.html', {'form': form})
 
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
-    except Category.DoesNotExist:
+    except:
         category = None
-
-    if category is None:
-        return redirect('/rango/')
     
+    # You cannot add a page to a Category that does not exist... DM
+    if category is None:
+        return redirect(reverse('rango:index'))
+
     form = PageForm()
 
     if request.method == 'POST':
@@ -74,7 +76,7 @@ def add_page(request, category_name_slug):
 
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         else:
-            print(form.errors)
-
+            print(form.errors)  # This could be better done; for the purposes of TwD, this is fine. DM.
+    
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
